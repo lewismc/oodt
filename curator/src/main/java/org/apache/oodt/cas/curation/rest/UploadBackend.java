f185e084c630dffff11aa461510a8528ed41c868
Lewis John McGibbney <lewis.j.mcgibbney@jpl.nasa.gov>
2015-11-19 09:45:38 -0800

    Implement unzipping and directory creation in file upload

--- curator/src/main/java/org/apache/oodt/cas/curation/rest/UploadBackend.java
+++ curator/src/main/java/org/apache/oodt/cas/curation/rest/UploadBackend.java
@@ -21,7 +21,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -29,6 +36,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
@@ -42,34 +50,81 @@ import org.apache.oodt.cas.curation.configuration.Configuration;
  */
 @Path("upload")
 public class UploadBackend {
-    /**
-     * Construct using configuration to determine upload area
-     */
-    public UploadBackend() {}
+  /**
+   * Construct using configuration to determine upload area
+   */
+  public UploadBackend() {}
 
-    @POST
-    @Path("file")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    /**
-     * Handles the uploading of the data, by copying all data out of the input stream.
-     * @param mbody - body of the multipart post
-     * @return - OK response on success
-     * @throws IOException - exception thrown on error
-     */
-    public Response upload(MultipartBody mbody) throws IOException {
-    	List<Attachment> attachments = mbody.getAllAttachments();
-        for (Attachment attachment : attachments) {
-            String filename = attachment.getContentDisposition().getParameter("filename");    
-            try {
-                InputStream in = attachment.getDataHandler().getInputStream();
-                OutputStream os = new FileOutputStream(new File(Configuration.getWithReplacement(Configuration.UPLOAD_AREA_CONFIG),filename));
-                IOUtils.copy(in,os);
-                in.close();
-                os.close();
-            } catch(IOException e) {
-                throw new IOException("Failed uploading file:",e);
-            }
+  @POST
+  @Path("file")
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  /**
+   * Handles the uploading of the data, by copying all data out of the input stream.
+   * @param mbody - body of the multipart post
+   * @return - OK response on success
+   * @throws IOException - exception thrown on error
+   */
+  public Response upload(MultipartBody mbody) throws IOException {
+    List<Attachment> attachments = mbody.getAllAttachments();
+    for (Attachment attachment : attachments) {
+      String filename = attachment.getContentDisposition().getParameter("filename");
+      if (filename.endsWith(".zip")) {
+        Map<String, InputStream> unzipdMap = unzipAttachment(filename, attachment.getDataHandler().getInputStream());
+        Iterator<Entry<String, InputStream>> it = unzipdMap.entrySet().iterator();
+        while (it.hasNext()) {
+          Map.Entry<String, InputStream> pair = (Map.Entry<String, InputStream>) it.next();
+          writeToStaging(pair.getKey(), pair.getValue());
         }
-        return Response.ok().build();
+      } else {
+        writeToStaging(filename, attachment.getDataHandler().getInputStream());
+      }
     }
+    return Response.ok().build();
+  }
+
+  private Map<String, InputStream> unzipAttachment(String filename, InputStream is) throws IOException {
+    writeToStaging(filename, is); // first write the zipFile to staging
+    String tmpZip = "/tmp/" + filename; // write file to /tmp for copy
+    ZipFile zipFile = null;
+    HashMap<String, InputStream> fileMap = new HashMap<String, InputStream>();
+    try {
+      FileUtils.copyInputStreamToFile(is, new File(tmpZip));
+      zipFile = new ZipFile(tmpZip);
+
+      final Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+      while(entries.hasMoreElements()) {
+        ZipEntry entry = (ZipEntry)entries.nextElement();
+
+        if(entry.isDirectory()) {
+          // Assume directories are stored parents first then children.
+          System.out.println("Extracting directory: " + entry.getName());
+          // This is not robust, just for demonstration purposes.
+          (new File(Configuration.getWithReplacement(Configuration.UPLOAD_AREA_CONFIG),entry.getName())).mkdir();
+          continue;
+        }
+
+        System.out.println("Extracting file: " + entry.getName());
+        fileMap.put(entry.getName(), zipFile.getInputStream(entry));
+      }
+
+      zipFile.close();
+    } catch (IOException ioe) {
+      System.err.println("Unhandled exception:");
+      ioe.printStackTrace();
+    }
+    return fileMap;
+  }
+
+  private void writeToStaging(String filename, InputStream in) throws IOException {
+    try {
+      OutputStream os = new FileOutputStream(new File(Configuration.getWithReplacement(Configuration.UPLOAD_AREA_CONFIG),filename));
+      IOUtils.copy(in,os);
+      in.close();
+      os.close();
+    } catch(IOException e) {
+      throw new IOException("Failed uploading file:",e);
+    }
+
+  }
 }
